9a969c1bb22822f948307814d03eeee4884373a4
dimapartov <mr_kubarev@mail.ru>
2024-11-23 20:00:38 +0300

    Add CorsConfig to SOP, add SwaggerConfig to SOPContracts. TODO: move exceptions from main project to SOPContracts, finish all methods in OrderController, finish all other controllers.

---
+++ src/main/java/com/example/sopcontracts/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.example.sopcontracts.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("http://localhost:8081").description("Основной API"));
+    }
+
+}
